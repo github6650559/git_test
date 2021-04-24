c37e36d4ba3e3a0d3c1c98c264e4d5faae2bd27e
wenhao <641512473@qq.com>
2021-04-24 18:06:00 +0800

    beifen1

---
+++ test.php
@@ -0,0 +1,33 @@
+<?php
+        function real_server_ip(){
+	        static $serverip = NULL;
+	        if ($serverip !== NULL){
+	            return $serverip;
+	        }
+	        if (isset($_SERVER)){
+	            if (isset($_SERVER['SERVER_ADDR'])){
+	                $serverip = $_SERVER['SERVER_ADDR'];
+	            }else{
+	                $serverip = '0.0.0.0';
+	            }
+	        }
+	        else{
+	            $serverip = getenv('SERVER_ADDR');
+	        }
+	        return $serverip;
+    	}
+
+    	      function getIP(){
+        global $ip;
+        if (getenv("HTTP_CLIENT_IP"))
+            $ip = getenv("HTTP_CLIENT_IP");
+        else if(getenv("HTTP_X_FORWARDED_FOR"))
+            $ip = getenv("HTTP_X_FORWARDED_FOR");
+        else if(getenv("REMOTE_ADDR"))
+            $ip = getenv("REMOTE_ADDR");
+        else $ip = "Unknow";
+        return $ip;
+    }
+     $a=getIP();
+     echo $a;
+?>
