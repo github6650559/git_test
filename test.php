7ee75afe1286d7b798006921663a406b0550e756
wenhao <641512473@qq.com>
2021-04-25 03:00:11 +0800

    fenzhibeifen1

--- test.php
+++ test.php
@@ -1,33 +1,3 @@
 <?php
-        function real_server_ip(){
-	        static $serverip = NULL;
-	        if ($serverip !== NULL){
-	            return $serverip;
-	        }
-	        if (isset($_SERVER)){
-	            if (isset($_SERVER['SERVER_ADDR'])){
-	                $serverip = $_SERVER['SERVER_ADDR'];
-	            }else{ss
-	                $serverip = '0.0.0.0';
-	            }
-	        }
-	        else{
-	            $serverip = getenv('SERVER_ADDR');
-	        }
-	        return $serverip;
-    	}
-
-    	      function getIP(){
-        global $ip;
-        if (getenv("HTTP_CLIENT_IP"))
-            $ip = getenv("HTTP_CLIENT_IP");
-        else if(getenv("HTTP_X_FORWARDED_FOR"))
-            $ip = getenv("HTTP_X_FORWARDED_FOR");
-        else if(getenv("REMOTE_ADDR"))
-            $ip = getenv("REMOTE_ADDR");
-        else $ip = "Unknow";
-        return $ip;
-    }
-     $a=getIP();
-     echo $a;
+        分界线
 ?>
